ee4f34c78830212b0bc57a3234a0999675ae28ab
nacocode <naco120692@gmail.com>
2020-02-20 21:21:27 -0500

    added monthly payment &  retrieve continue answer parts

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -30,17 +30,60 @@ def retrieve_number(msg)
     elsif number.to_f < 0 || number == "0"
       prompt(messages("greater_than_zero"))
     elsif valid_number(number)
-      return number
+      return number.to_f
     else
       prompt(messages("error_invalid_input"))
     end
   end
 end
 
-prompt("Welome to the mortgage calculator!")
+def year_to_month(number)
+  number.to_i * 12
+end
+
+def continue?(answer)
+  loop do
+    prompt(messages("continue"))
+    answer = Kernel.gets().chomp().downcase
+    if answer.empty?
+      prompt(messages("error_blank_input"))
+    elsif answer != "yes" && answer != "no"
+      prompt(messages("error_invalid_input"))
+    else
+      return answer
+    end
+  end
+end
+
+def clear_screen
+  system("clear") || system("cls")
+end
+
+#---Main---------------------------------------------------------
+prompt("Welome to the Mortgage Calculator!")
 prompt("-----------------------------------")
 
-amount = retrieve_number("retrieve_loan_amount")
-interest = retrieve_number("retrieve_interest_rate")
+loop do
+  mortgage_amount = retrieve_number("retrieve_mortgage_amount")
+  annual_interest_rate = retrieve_number("retrieve_interest_rate")
+  duration = retrieve_number("retrieve_loan_duration")
+  month_duration = year_to_month(duration)
+  monthly_interest_rate = (annual_interest_rate.to_f / 12)
 
-# prompt("What is the loan duration?")
+  prompt("Mortgage amount is $#{mortgage_amount}")
+  prompt("interest is #{annual_interest_rate}%")
+  prompt("duration is #{duration} years")
+  prompt("month duration is #{month_duration} months")
+  prompt("monthly interest rate is #{monthly_interest_rate.round(2)}%")
+
+  monthly_payment = mortgage_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-month_duration)))
+
+  prompt("monthly payment is $#{monthly_payment.round(2)}")
+
+  continue_answer = continue?(continue_answer)
+
+  clear_screen if continue_answer == "yes"
+  break if continue_answer == "no"
+end
+
+prompt(messages("good_bye"))
