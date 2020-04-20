204f348261ba6a3023ece9b02310113cebbc73bf
nacocode <naco120692@gmail.com>
2020-04-20 11:58:16 -0400

    Reviewed the question

--- Lesson_3/easy2_q8.rb
+++ Lesson_3/easy2_q8.rb
@@ -1,2 +1,6 @@
 advice = "Few things in life are as important as house training your pet dinosaur."
-p advice.index('house')
+p advice.index("house") # =ｐ> 39
+p advice.slice(0, advice.index("house"))
+p advice
+p advice.slice!(0, advice.index("house"))
+p advice
